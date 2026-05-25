30868afec1e05d8a5266bd431975405bd31e0d57
Oussama Yaqdane <rakaoran@gmail.com>
2026-05-25 22:38:19 +0100

    unused vars aren't shadowed

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,7 +2,15 @@ return {
 	"folke/tokyonight.nvim",
 	lazy = false,
 	priority = 1000,
-	opts = { style = "night" },
+	opts = {
+		style = "night",
+		on_highlights = function(highlights)
+			highlights.DiagnosticUnnecessary = {}
+			highlights["@lsp.mod.unused"] = {}
+			highlights["@lsp.typemod.variable.unused"] = {}
+			highlights["@lsp.typemod.parameter.unused"] = {}
+		end,
+	},
 	config = function(_, opts)
 		require("tokyonight").setup(opts)
 		vim.cmd.colorscheme("tokyonight")
